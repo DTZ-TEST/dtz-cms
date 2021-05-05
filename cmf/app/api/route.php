29fd65d7f75ef8ce00f5b5847c222aebec90c46b
Dtz_2020 <cuciz7@163.com>
2021-05-05 13:05:11 +0800

    11

--- cmf/app/api/route.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-use think\facade\Route;
-
-Route::resource('api/articles', 'api/Articles');
--- cmf/data/runtime/cache/ad/beed380c67b504412a25e8c84ae90f.php
+++
@@ -1,4 +0,0 @@
-<?php
-//000000000000
- exit();?>
-a:3:{i:0;s:84:"D:\phpstudy_pro\WWW\dtz\cmf\data\runtime\cache\5d\488968ca96416815ab34c96e65cf3a.php";i:1;s:84:"D:\phpstudy_pro\WWW\dtz\cmf\data\runtime\cache\7c\fd7023fe239ca7e38dac18fc978562.php";i:2;s:84:"D:\phpstudy_pro\WWW\dtz\cmf\data\runtime\cache\4a\ca300412fd70b91f69939f531e34e3.php";}
