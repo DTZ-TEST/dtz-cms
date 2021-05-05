e3d3193a884333736e93d686358dbc1a55b2f911
Dtz_2020 <cuciz7@163.com>
2021-05-05 12:36:31 +0800

    aaa

---
+++ cmf/app/demo/controller/IndexController.php
@@ -0,0 +1,34 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkCMF [ WE CAN DO IT MORE SIMPLE ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2013-present http://www.thinkcmf.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Released under the MIT License.
+// +----------------------------------------------------------------------
+// | Author: 老猫 <thinkcmf@126.com>
+// +----------------------------------------------------------------------
+
+namespace app\demo\controller;
+
+use cmf\controller\HomeBaseController;
+
+class IndexController extends HomeBaseController
+{
+    public function index()
+    {
+        echo 404;
+        exit;
+        return $this->fetch(':index');
+    }
+
+    public function block()
+    {
+        return $this->fetch();
+    }
+
+    public function ws()
+    {
+        return $this->fetch(':ws');
+    }
+}
